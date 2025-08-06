5ce800e51da614da55f99968988fd5cd2a1d5015
vitaliylysikov <84893623+vitaliylysikov@users.noreply.github.com>
2025-08-06 12:54:16 +0800

    Update twrp_giulia.mk

--- twrp_giulia.mk
+++ twrp_giulia.mk
@@ -10,17 +10,17 @@ DEVICE_PATH := device/oneplus/giulia
 $(call inherit-product, $(DEVICE_PATH)/device.mk)
 
 # Release name
-PRODUCT_RELEASE_NAME := OnePlus Ace 5
+PRODUCT_RELEASE_NAME := giulia
 
 ## Device identifier
 PRODUCT_DEVICE := giulia
 PRODUCT_NAME := twrp_giulia
 PRODUCT_BRAND := OnePlus
-PRODUCT_MODEL := PKG110
+PRODUCT_MODEL := giulia
 PRODUCT_MANUFACTURER := OnePlus
 
 # Assert
-TARGET_OTA_ASSERT_DEVICE := PKG110,OP5D2BL1
+TARGET_OTA_ASSERT_DEVICE := PKG110,OP5D2BL1,CPH2691,OP5D3BL1,CPH2647,OP5D3BL1
 
 # Theme
 TW_STATUS_ICONS_ALIGN := center
