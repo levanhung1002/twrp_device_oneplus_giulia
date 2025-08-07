aef0c30c4a664dae7ec2998e28abb0ec4ddcea22
vitaliylysikov <84893623+vitaliylysikov@users.noreply.github.com>
2025-08-07 21:18:52 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -41,6 +41,3 @@ PRODUCT_SOONG_NAMESPACES += \
 # Touch features
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_GM,true)
 $(call soong_config_set,OPLUS_LINEAGE_TOUCH_HAL,ENABLE_HTPR,false)
-
-# Vibrator
-$(call soong_config_set,OPLUS_LINEAGE_VIBRATOR_HAL,INCLUDE_DIR,$(LOCAL_PATH)/vibrator/include)
