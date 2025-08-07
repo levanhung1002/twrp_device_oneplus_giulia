bbe1f1e38745b00a05f1a7d95535462449639106
vitaliylysikov <84893623+vitaliylysikov@users.noreply.github.com>
2025-08-07 18:00:50 +0800

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_giulia.mk
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_giulia.mk
 
-COMMON_LUNCH_CHOICES := twrp_giulia-eng
+COMMON_LUNCH_CHOICES := \
+    twrp_giulia-user \
+    twrp_giulia-userdebug \
+    twrp_giulia-eng
